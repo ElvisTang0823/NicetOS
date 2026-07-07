0403c995bad65d07e99c3ad2e9eeb22d8f1e8c15
ElvisTang0823 <elvis990823@gmail.com>
2026-07-07 21:35:16 +0800

    多寫了一些註解，並且整理整理架構

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # 導入函式庫
 import json
-
-# 拆解網址：分成網域、路徑、參數等
 from urllib.parse import urlparse
 
+# ==========================
+# 拆解網址：分成網域、路徑、參數等，只留下最乾淨的網址
 def extract_domain(input_link: str) -> str:
     if '://' not in input_link:
         input_link = 'http://' + input_link
     netloc = urlparse(input_link).netloc
     return netloc.split(':')[0].rstrip('.')
 
-# ======1====================
+# ===========================
 # 讀取白名單
 with open('data/whitelist.json', 'r', encoding='utf-8') as file:
     whitelist = json.load(file)
@@ -21,6 +21,7 @@ with open('data/blacklist.json', 'r', encoding='utf-8') as file:
 # ==========================
 # 邏輯判斷的function：在白名單內為真/黑名單內為假/皆非則回傳0，接著讓Go判斷是否允許訪問
 # 目前問題：若白名單與黑名單同時存在，會回傳True
+# 目前問題2：時間複雜度O(n)，未來改成使用Trie樹或HashSet
 def check_url(input_link):
     for url in whitelist:
         if url in input_link:
